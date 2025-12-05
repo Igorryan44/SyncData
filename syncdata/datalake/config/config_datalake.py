cd952340b9802d7749d9c8c66c2d757753a42a71
Igor Ryan <igorryanmendesalves@gmail.com>
2025-12-04 22:03:44 -0300

    Datalake com MinIO para upload dos dados

---
+++ syncdata/datalake/config/config_datalake.py
@@ -0,0 +1,24 @@
+# file_uploader.py MinIO Python SDK example
+from minio import Minio
+from minio.error import S3Error
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
+SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
+
+def create_conection():
+    # Criar um cliente MinIO com o endpoint, access key e secret key
+    client = Minio(
+        endpoint="play.min.io",
+        access_key=ACCESS_KEY,
+        secret_key=SECRET_KEY,
+    )
+    return client
+
+if __name__ == "__main__":
+    try:
+        create_conection()
+    except S3Error as exc:
+        print("Erro: ", exc)
---
+++ syncdata/datalake/upload/upload_datalake_file.py
@@ -0,0 +1,42 @@
+from minio import Minio
+from minio.error import S3Error
+from config.config_datalake import create_conection
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+PROJECT_PATH = Path('__file__').absolute().parent
+logger.info(PROJECT_PATH)
+
+def upload_file(client, file):
+    # Arquivo a ser enviado
+    source_file = f"{PROJECT_PATH}/syncdata/data/{file}"
+    # Destino de envio no bucket
+    bucket_name = "syncdata"
+    destination_file = f"{file}"
+    
+    # Criar o bucket caso não exista
+    found = client.bucket_exists(bucket_name=bucket_name)
+    if not found:
+        client.make_bucket(bucket_name=bucket_name)
+        logger.info("Bucket criado %s", bucket_name)
+    else:
+        logger.info("Bucket %s já existe", bucket_name)
+
+    # Enviar o arquivo, renomeando-o no processo
+    client.fput_object(
+        bucket_name=bucket_name,
+        object_name=destination_file,
+        file_path=source_file,
+    )
+    logger.info(f"O dataset {source_file} enviado com sucesso como objeto para o bucket {bucket_name}")
+    
+
+if __name__ == "__main__":
+    try:
+        minio_client = create_conection()
+        upload_file(minio_client, "cms_inpatient.csv")
+    except S3Error as exc:
+        logger.error(f"Erro: {exc}")
