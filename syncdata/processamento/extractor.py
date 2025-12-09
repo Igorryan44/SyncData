ea72389685e84c83528c65f58f1ab9b3d0f5052e
Igor Ryan <igorryanmendesalves@gmail.com>
2025-12-09 18:48:10 -0300

    Tema escolhido: Acidentes de Trabalho

--- syncdata/processamento/extractor.py
+++ syncdata/processamento/extractor.py
@@ -1,75 +1,38 @@
-import requests
-import logging
 import polars as pl
+from pysus import SINAN
 from pathlib import Path
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry
+import logging
 
-FILE_PATH = Path('__file__').parent.absolute().parent
-
-# Configurações de log
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("Processor")
+logger = logging.getLogger("extrator")
 
-# Recursos API
-url = 'https://data.cms.gov/data.json'
-headers = {'accept': 'application/json'}
+data_path = Path(__name__).parent.absolute().parent / "datasets"
 
-# Função principal
-def main(api_url, api_headers):
-    logger.info("Iniciando extração dos dados da API CMS")
-    data = requests.get(url=api_url, headers=api_headers).json()
+logger.info("Iniciando extração dos dados do SINAN - Acidente de Trabalho (ACGR)")
 
-    logger.info("Criando DataFrame inicial")
-    root_df = pl.DataFrame(data["dataset"], strict=False, nan_to_null=True)
-    logger.info("DataFrame inicial criado com sucesso")
-    
-    logger.info("Transformando dados")
-    root_df = root_df.explode("distribution"
-                                ).explode("theme"
-                                ).explode("references"
-                                ).explode("programCode"
-                                ).explode("language"
-                                ).explode("keyword"
-                                ).explode("bureauCode"
-                                ).unnest(columns=("contactPoint","distribution", "publisher"), separator="_"
-                                )
+# Configuração e extração dos dados do SINAN - Acidente de Trabalho (ACGR)
+sinan = SINAN().load()
+years = [i for i in range(2025) if i >1999]
 
-    logger.info("Dados transformados com sucesso")
-    logger.info("Explorando URLs de distribuição para 'Inpatient'")
+# Extração dos dados de Acidente de Trabalho de 2000 a 2025
+files = sinan.get_files(dis_code="ACGR", year=years)
 
-    lf_group = root_df.group_by("keyword").agg(pl.col("distribution_accessURL"))
+logger.info(f"Total de arquivos a serem baixados: {len(files)}")
+logger.info(f"Iniciando download dos arquivos...")
+# Download dos arquivos
+sinan.download(files, local_dir="data/")
 
+logger.info("Download concluído. Iniciando leitura e concatenação dos arquivos...")
+# Concatenação dos dataframes
+dfs = []
 
-    lf_count_values = lf_group.filter(pl.col("keyword") == "Inpatient")
-    lf_count_values = lf_count_values.explode("distribution_accessURL").select("distribution_accessURL")
-
-    logger.info("Preparando requisições para as APIs encontradas")
-    lista_de_apis = []
-
-    for api in lf_count_values["distribution_accessURL"]:
-        if not api == None:
-            lista_de_apis.append(api)
-    total_apis = len(lista_de_apis)
-    dfs = []
-    cont = 1
-
-    logger.info(f"{total_apis} APIs encontradas para requisição")
-    logger.info(f"Iniciando requisições...")
-    for api in lista_de_apis:
-        data = requests.get(url=api, headers=headers)
-        logger.info(f"Requisições realizadas: {cont}/{total_apis}")
-        df = pl.DataFrame(data.json(), strict=False, nan_to_null=True)
+for file in data_path.rglob("*.parquet"):
+    if file.is_file():
+        df = pl.read_parquet(file)
         dfs.append(df)
-        cont += 1
 
-    logger.info("Todas as requisições foram realizadas com sucesso")    
+df_final = pl.concat(dfs, rechunk=True)
+df_final.write_parquet(f"{data_path}ACGRBR.parquet")
 
-    logger.info("Concatenando DataFrames obtidos")
-    final_df = pl.concat(dfs, how="diagonal", rechunk=True)
-    logger.info("Salvando arquivo cms_inpatient.parquet")
-    final_df.write_parquet(f"{FILE_PATH}/SyncData/syncdata/data/cms_inpatient.parquet")
-
-if __name__ == "__main__":
-    main(api_url=url, api_headers=headers)
+logger.info("Extração e concatenação concluídas com sucesso.")
+logger.info(f"Arquivo final salvo em: {data_path / 'ACGRBR.parquet'}")
